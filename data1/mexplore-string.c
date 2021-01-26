da642a07f4ffe29ce9c186e01febc9739081ffd7
Malte Schwarzkopf <malte@cs.brown.edu>
2021-01-26 11:45:18 -0500

    Clearer string intro

--- data1/mexplore-string.c
+++ data1/mexplore-string.c
@@ -3,10 +3,10 @@
 #include <assert.h>
 #include "hexdump.h"
 
-char* global_st = "Hello CS131!";
+char global_st[] = "Hello CS131!";
 
 void f() {
-    char* local_st = "We <3 systems";
+    char local_st[] = "We <3 systems";
 
     //char* allocated_st = (char*)malloc(1);
     //sprintf(allocated_st, "C programming is cool");
