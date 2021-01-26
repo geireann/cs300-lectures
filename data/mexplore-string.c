f8d363d45382109629559f5f7da5570acbc504e3
Malte Schwarzkopf <malte@cs.brown.edu>
2021-01-26 09:35:41 -0500

    Add mexplore + fix Makefile

---
+++ data/mexplore-string.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <assert.h>
+#include "hexdump.h"
+
+char* global_st = "Hello CS131!";
+
+void f() {
+    char* local_st = "We <3 systems";
+
+    //char* allocated_st = (char*)malloc(1);
+    //sprintf(allocated_st, "C programming is cool");
+
+    hexdump(global_st, 11);
+    hexdump(local_st, 13);
+    //hexdump(allocated_st, strlen(allocated_st));
+}
+
+int main() {
+    f();
+}
