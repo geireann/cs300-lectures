9194bcce20299b319497aee2e583dc7587741db8
Malte Schwarzkopf <malte@cs.brown.edu>
2021-01-28 10:33:55 -0500

    Tweaks

--- data1/ptr-intro.c
+++ data1/ptr-intro.c
@@ -2,15 +2,15 @@
 
 void f() {
     int local = 1;
-    int* ptr = &local;
 
     printf("value of local: %d\n", local);
-
     printf("address of local: %p\n", &local);
+
+    int* ptr = &local;
 
     printf("value of ptr: %p\n", ptr);
 
-    printf("value of *ptr: %d\n", *ptr);
+    //printf("value of *ptr: %d\n", *ptr);
 }
 
 int main(int argc, char* argv[]) {
