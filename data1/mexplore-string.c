6169c72812a40e4ce4a449e6ebbb36192cb23ec5
Malte Schwarzkopf <malte@cs.brown.edu>
2021-01-28 16:00:04 +0000

    Tweak

--- data1/mexplore-string.c
+++ data1/mexplore-string.c
@@ -8,11 +8,12 @@ char global_st[] = "Hello CS131!";
 void f() {
     char local_st[] = "We <3 systems";
 
+    hexdump(global_st, 11);
+    hexdump(local_st, 13);
+
     //char* allocated_st = (char*)malloc(1);
     //sprintf(allocated_st, "C programming is cool");
 
-    hexdump(global_st, 11);
-    hexdump(local_st, 13);
     //hexdump(allocated_st, strlen(allocated_st));
 }
 
