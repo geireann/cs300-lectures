a5f8f6eeaea8b56d32e87147a804cb99b41865b0
Malte Schwarzkopf <malte@cs.brown.edu>
2021-02-04 16:40:37 +0000

    Array updates

--- data1/mexplore-array.c
+++ data1/mexplore-array.c
@@ -12,15 +12,21 @@ void f() {
     //printf("--------\n");
 
     // other array syntax
-    //int arr2[5];
-    //
-    //arr2[0] = 1;
-    //arr2[1] = 2;
-    //arr2[2] = 128;
-    //arr2[3] = 17;
-    //arr2[4] = 0;
-    //
-    //hexdump(arr2, sizeof(arr2));
+    int arr2[5];
+
+    arr2[0] = 1;
+    arr2[1] = 2;
+    arr2[2] = 128;
+    arr2[3] = 17;
+    arr2[4] = 0;
+
+    hexdump(arr2, sizeof(arr2));
+
+    printf("%d\n", sizeof(int));
+    printf("%d\n", sizeof(char));
+    printf("%d\n", sizeof(char*));
+    printf("%d\n", sizeof(arr));
+    printf("%d\n", sizeof((int*)arr));
 }
 
 int main() {
